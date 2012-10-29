3f8e470843bb1283072fc65ce667fd2710a6ac5e
lisa ballard <lballard.cat@gmail.com>
2012-10-29 11:47:13 -0700

    changed a var name

--- LED_solar_wind.py
+++ LED_solar_wind.py
@@ -5,31 +5,31 @@ import urllib2
 # initiate the LED
 GPIO.setup(11, GPIO.OUT)
 
-def blink(speed):
-	blink_delay = get_blink_delay(speed)
+def blink(wind_speed):
+	blink_delay = get_blink_delay(wind_speed)
 	GPIO.output(11, True)
 	time.sleep(blink_delay)
 	GPIO.output(11, False)
 	time.sleep(blink_delay)
 
-def get_blink_delay(wind_speed):
-	# want to find blink delay that corresponds inversely with wind speed
-	# so when wind speed increases blink speed increases
-	# blink delay is time between ON and OFF and corresponds inversely with solar wind speed
+def get_blink_delay(wind_wind_speed):
+	# want to find blink delay that corresponds inversely with wind wind_speed
+	# so when wind wind_speed increases blink wind_speed increases
+	# blink delay is time between ON and OFF and corresponds inversely with solar wind wind_speed
 	# so doing this with a negative linear slope between 2 points on a plane (my WAG at an approach)
-	# where x axis = wind speed and y axis = blink speed
+	# where x axis = wind wind_speed and y axis = blink wind_speed
 	# find the equation of the slope between the 2 points then 
-	# when given wind_speed solve for y to get blink delay:
+	# when given wind_wind_speed solve for y to get blink delay:
 
 	# for now we are manually guessing at endpoints, based on perusing the data:
-	wind_speed_endpoints = (260.,325) # x1, x2
+	wind_wind_speed_endpoints = (260.,325) # x1, x2
 	blink_delay_endpoints = (2., .05)  # y1, y2
 
 	# solve for y to get blink delay
-	(x1, x2) = wind_speed_endpoints  # points on an x-y graph
+	(x1, x2) = wind_wind_speed_endpoints  # points on an x-y graph
 	(y1, y2) = blink_delay_endpoints
 	m = (y2-y1)/(x2-x1) # slope = rise/run
-	blink_delay = m*(wind_speed - x1) + y1 # y-y1 = m(x-x1) maths! 
+	blink_delay = m*(wind_wind_speed - x1) + y1 # y-y1 = m(x-x1) maths! 
 	return blink_delay
 
 while True:
@@ -49,7 +49,7 @@ while True:
 		data = line.split()
 
 		try:
-			my_science_data.append({'SDAY': data[-5], 'Bulk_Speed' : data[-2] })
+			my_science_data.append({'SDAY': data[-5], 'Bulk_wind_speed' : data[-2] })
 			page_lines.close()
 			break # we only want the first real data entry
 		except IndexError:	
@@ -62,23 +62,23 @@ while True:
 	offset = 20  # offset in seconds - 
 	for obs in my_science_data:
 
-		speed = float(obs['Bulk_Speed'])
+		wind_speed = float(obs['Bulk_wind_speed'])
 
 		"""
 		print obs
-		print speed
-		print get_blink_delay(speed)
+		print wind_speed
+		print get_blink_delay(wind_speed)
 		"""
 		now = time.time()
 		future = now + offset
 
-		if speed == -9999.9: # this is their null, just turn off the LED..
+		if wind_speed == -9999.9: # this is their null, just turn off the LED..
 			GPIO.output(11, False)
 			time.sleep(int(offset/2))
 			break
 
 		while True:
-			blink(speed)
+			blink(wind_speed)
 			if time.time() > future:
 				break			
 
