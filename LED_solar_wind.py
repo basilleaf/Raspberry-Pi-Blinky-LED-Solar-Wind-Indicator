1e1a1ec4c91bc4af9a86b9ca815b22016cae9bea
lisa ballard <lballard.cat@gmail.com>
2012-10-29 11:40:42 -0700

    doc love

--- LED_solar_wind.py
+++ LED_solar_wind.py
@@ -1,7 +1,6 @@
 """
-	Blink an LED at speeds that correspond with real time solar wind speed
-	wind speed data here: http://www.swpc.noaa.gov/ftpdir/lists/ace/ace_swepam_1m.txt
-	For the Raspberry Pi.
+	For the Raspberry Pi: Blink an LED to indicate speed of solar wind speed
+	real time solar wind data feed here: http://www.swpc.noaa.gov/ftpdir/lists/ace/ace_swepam_1m.txt
 """
 import RPi.GPIO as GPIO
 import time
@@ -11,29 +10,30 @@ import urllib2
 GPIO.setup(11, GPIO.OUT)
 
 def blink(speed):
-	blink_delay = find_blink_delay(speed)
+	blink_delay = get_blink_delay(speed)
 	GPIO.output(11, True)
 	time.sleep(blink_delay)
 	GPIO.output(11, False)
 	time.sleep(blink_delay)
 
-def find_blink_delay(wind_speed):
+def get_blink_delay(wind_speed):
 	# want to find blink delay that corresponds inversely with wind speed
-	# so when wind speed increases blink delay becomes proportionally smaller
+	# so when wind speed increases blink speed increases
+	# blink delay is time between ON and OFF and corresponds inversely with solar wind speed
 	# so doing this with a negative linear slope between 2 points on a plane (my WAG at an approach)
 	# where x axis = wind speed and y axis = blink speed
-	# so find the equation of the slope between xmin, ymin and xmax, ymax
-	# so then when given wind_speed you just solve for y to get blink deley:
+	# find the equation of the slope between the 2 points then 
+	# when given wind_speed solve for y to get blink delay:
 
 	# for now we are manually guessing at endpoints, based on perusing the data:
-	wind_speed_endpoints = (300.,360) # x1, x2
-	blink_delay_endpoints = (2., .1)  # y1, y2
+	wind_speed_endpoints = (260.,325) # x1, x2
+	blink_delay_endpoints = (2., .05)  # y1, y2
 
 	# solve for y to get blink delay
 	(x1, x2) = wind_speed_endpoints  # points on an x-y graph
 	(y1, y2) = blink_delay_endpoints
-	m = (y2-y1)/(x2-x1) # slope
-	blink_delay = m*(wind_speed - x1) + y1
+	m = (y2-y1)/(x2-x1) # slope = rise/run
+	blink_delay = m*(wind_speed - x1) + y1 # y-y1 = m(x-x1) maths! 
 	return blink_delay
 
 while True:
@@ -60,18 +60,23 @@ while True:
 			pass
 
 	# start the sequence
-	offset = 20 # seconds - the real offset is 60
+	
+	# the real offset should be 60 but noticing the data actually changes in shorter increments *shrug* 
+	# so hitting the data feed page every 20 seconds.. 
+	offset = 20  # offset in seconds - 
 	for obs in my_science_data:
 
 		speed = float(obs['Bulk_Speed'])
+
+		"""
 		print obs
 		print speed
-		print find_blink_delay(speed)
-
+		print get_blink_delay(speed)
+		"""
 		now = time.time()
 		future = now + offset
 
-		if speed == -9999.9: # this is their null, turn it off..
+		if speed == -9999.9: # this is their null, just turn off the LED..
 			GPIO.output(11, False)
 			time.sleep(int(offset/2))
 			break
